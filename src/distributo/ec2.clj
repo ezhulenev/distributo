210d51cca76f23f19fd2b78bd6caa5da94cf7fbf
ezhulenev <evgeny.zhulenev@gmail.com>
2016-01-17 23:21:52 -0500

    More api calls for ec2/ecs

--- src/distributo/ec2.clj
+++ src/distributo/ec2.clj
@@ -4,10 +4,14 @@
             [clojure.string :as str]
             [base64-clj.core :as base64]
             [distributo.util :refer :all])
-  (:import (com.amazonaws.services.ec2.model LaunchSpecification IamInstanceProfileSpecification RequestSpotInstancesRequest SpotInstanceRequest)
+  (:import (com.amazonaws.services.ec2.model LaunchSpecification IamInstanceProfileSpecification RequestSpotInstancesRequest SpotInstanceRequest CreateTagsRequest Tag DescribeSpotInstanceRequestsRequest Filter SpotInstanceType)
            (com.amazonaws.services.ec2 AmazonEC2Client)
            (com.amazonaws.auth AWSCredentialsProvider)
            (com.amazonaws.regions Region Regions)))
+
+;; Tag EC2 objects with cluster name
+
+(def cluster-tag-name "DistributoCluster")
 
 ;; Mapping for AWS ECS enabled AMIs
 
@@ -20,33 +24,13 @@
           :ap-southeast-1 "ami-c78f43a4"
           :ap-southeast-2 "ami-43547120"})
 
-(def instance-defaults {:region         :us-east-1
-                        :instance-type  :m4.large
-                        :security-group "default"
-                        :iam-role       "ecsInstanceRole"
-                        :key-name       "default"})
+(def launch-specification-defaults {:region         :us-east-1
+                                    :instance-type  :m4.large
+                                    :security-group "default"
+                                    :iam-role       "ecsInstanceRole"
+                                    :key-name       "default"})
 
-(defn new-client
-  ([^AWSCredentialsProvider cred]
-   (AmazonEC2Client. cred))
-  ([^AWSCredentialsProvider cred region]
-   (-> (AmazonEC2Client. cred)
-       (.setRegion (Region/getRegion (Regions/fromName region))))))
-
-(defn launch-specification
-  ([cluster] (launch-specification cluster {}))
-  ([cluster opts]
-   (let [{:keys [region instance-type security-group iam-role key-name]} (merge instance-defaults opts)
-         user-data (str/join ["#!/bin/bash" "\n"
-                              "echo ECS_CLUSTER=" cluster " >> /etc/ecs/ecs.config"])]
-     (-> (LaunchSpecification.)
-         (.withImageId (region ami))
-         (.withInstanceType (name instance-type))
-         (.withSecurityGroups [security-group])
-         (.withKeyName key-name)
-         (.withUserData (base64/encode user-data))
-         (.withIamInstanceProfile (-> (IamInstanceProfileSpecification.)
-                                      (.withName iam-role)))))))
+;; Mapping from AWS SDK objects into clojure data structures
 
 (defn spot-instances-request->map
   [^SpotInstanceRequest req]
@@ -64,27 +48,80 @@
      :instance-id              instance-id
      }))
 
-(defn spot-instances-request
-  [price n spec]
-  (-> (RequestSpotInstancesRequest.)
-      (.withSpotPrice (.toString price))
-      (.withInstanceCount (int n))
-      (.withLaunchSpecification spec)))
+;; Construct new AWS SDK objects
+
+(defn new-client
+  ([^AWSCredentialsProvider cred]
+   (AmazonEC2Client. cred))
+  ([^AWSCredentialsProvider cred region]
+   (-> (AmazonEC2Client. cred)
+       (.setRegion (Region/getRegion (Regions/fromName region))))))
+
+(defn new-launch-specification
+  ([cluster] (new-launch-specification cluster {}))
+  ([cluster opts]
+   (let [{:keys [region instance-type security-group iam-role key-name]} (merge launch-specification-defaults opts)
+         user-data (str/join ["#!/bin/bash" "\n"
+                              "echo ECS_CLUSTER=" cluster " >> /etc/ecs/ecs.config"])]
+     (-> (LaunchSpecification.)
+         (.withImageId (region ami))
+         (.withInstanceType (name instance-type))
+         (.withSecurityGroups [security-group])
+         (.withKeyName key-name)
+         (.withUserData (base64/encode user-data))
+         (.withIamInstanceProfile (-> (IamInstanceProfileSpecification.)
+                                      (.withName iam-role)))))))
+
+(defn new-spot-instances-request
+  [cluster opts]
+  (let [{:keys [spot-price count launch-specification]} opts
+        spec (new-launch-specification cluster launch-specification)]
+    (-> (RequestSpotInstancesRequest.)
+        (.withSpotPrice (.toString spot-price))
+        (.withType (SpotInstanceType/Persistent))
+        (.withInstanceCount (int count))
+        (.withLaunchSpecification spec))))
+
+;; AWS SDK EC2 Api
 
 (defn request-spot-instances
-  [^AmazonEC2Client client req]
-  (log/debug "Request spot instance:" req)
-  (-> client (.requestSpotInstances req)))
+  [^AmazonEC2Client client cluster request]
+  (log/debug "Request spot instance. Cluster:" cluster "Request:" request)
+  (let [ec2-spot-request (new-spot-instances-request cluster request)]
+    (map spot-instances-request->map
+         (-> client
+             (.requestSpotInstances ec2-spot-request)
+             (.getSpotInstanceRequests)))))
+
+(defn tag-spot-instance-requests
+  [^AmazonEC2Client client cluster requests]
+  (log/debug "Tag spot requests with cluster name:" cluster
+             "Requests:" (mapv :spot-instance-request-id requests))
+  (let [tag (Tag. cluster-tag-name cluster)
+        ec2-tag-request (-> (CreateTagsRequest.)
+                            (.withResources (map :spot-instance-request-id requests))
+                            (.withTags [tag]))]
+    (-> client (.createTags ec2-tag-request))))
 
 (defn describe-spot-requests
-  "Get spot requests state map"
-  [^AmazonEC2Client client]
+  [^AmazonEC2Client client cluster]
+  (let [tag-filter (-> (Filter.)
+                       (.withName (str "tag:" cluster-tag-name))
+                       (.withValues [cluster]))
+        ec2-describe-request (-> (DescribeSpotInstanceRequestsRequest.)
+                                 (.withFilters [tag-filter]))]
+    (map spot-instances-request->map
+         (-> client
+             (.describeSpotInstanceRequests ec2-describe-request)
+             (.getSpotInstanceRequests)))))
+
+;; Combinators on top of low lever AWS SDK
+
+(defn spot-requests-to-map
+  "Converts seq of spot requests into map"
+  [requests]
   (let [req->tuple (fn [req] [(keyword (:spot-instance-request-id req)) (dissoc req :spot-instance-request-id)])]
-    (into {} (map req->tuple
-                  (->> client
-                       (.describeSpotInstanceRequests)
-                       (.getSpotInstanceRequests)
-                       (map spot-instances-request->map))))))
+    (into {} (map req->tuple requests))))
 
 (defn spot-requests-transition
   "Cumpute transition map between two spot requests states"
@@ -95,21 +132,24 @@
 
 (defn spot-requests-chan
   "Constructs core.async channel with spot requests states"
-  [^AmazonEC2Client client metronome]
+  [^AmazonEC2Client client cluster metronome]
   (let [requests (chan)
-        xf (map (fn [_] (describe-spot-requests client)))]
-    (async/pipeline 1 requests xf metronome)
+        xf (comp
+             (map (fn [_] (describe-spot-requests client cluster)))
+             (map spot-requests-to-map))]
+    (async/pipeline-blocking 1 requests xf metronome)
     requests))
 
 (defn spot-requests-transitions-chan
   "Constructs core.async channel with spot requests transitions"
-  [^AmazonEC2Client client metronome]
+  [^AmazonEC2Client client cluster metronome]
   (let [transitions (chan)
         sliding-window (make-sliding-window 2)
         xf (comp
-             (map (fn [_] (describe-spot-requests client)))
+             (map (fn [_] (describe-spot-requests client cluster)))
+             (map spot-requests-to-map)
              (map (fn [state] (seq (sliding-window state))))
              (filter #(= 2 (count %)))
              (map (fn [[a b]] (spot-requests-transition a b))))]
-    (async/pipeline 1 transitions xf metronome)
+    (async/pipeline-blocking 1 transitions xf metronome)
     transitions))
--- src/distributo/ecs.clj
+++ src/distributo/ecs.clj
@@ -5,17 +5,15 @@
             [clojure.data :refer [diff]]
             [distributo.util :refer :all])
   (:import (com.amazonaws.services.ecs AmazonECSClient)
-           (com.amazonaws.services.ecs.model CreateClusterRequest DescribeClustersRequest DeleteClusterRequest Cluster)
+           (com.amazonaws.services.ecs.model CreateClusterRequest DescribeClustersRequest DeleteClusterRequest Cluster DescribeTaskDefinitionRequest TaskDefinition ContainerDefinition RegisterTaskDefinitionRequest)
            (com.amazonaws.auth AWSCredentialsProvider)
            (com.amazonaws.regions Region Regions)))
 
+;; Name of default container inside task definition
 
-(defn new-client
-  ([^AWSCredentialsProvider cred]
-   (AmazonECSClient. cred))
-  ([^AWSCredentialsProvider cred region]
-   (-> (AmazonECSClient. cred)
-       (.setRegion (Region/getRegion (Regions/fromName region))))))
+(def default-container-name "default")
+
+;; Mapping from AWS SDK objects into clojure data structures
 
 (defn cluster->map
   [^Cluster cluster]
@@ -31,29 +29,64 @@
      :running-tasks   running-tasks
      :pending-tasks   pending-tasks
      :active-services active-services
-     })
-  )
+     }))
+
+(defn container-definition->map
+  [^ContainerDefinition container-definition]
+  (let [name (-> container-definition (.getName))
+        image (-> container-definition (.getImage))
+        cpu (-> container-definition (.getCpu))
+        memory (-> container-definition (.getMemory))]
+    {:name   name
+     :image  image
+     :cpu    cpu
+     :memory memory}))
+
+(defn task-definition->map
+  [^TaskDefinition task-definition]
+  (let [family (-> task-definition (.getFamily))
+        revision (-> task-definition (.getRevision))
+        status (-> task-definition (.getStatus))
+        container-definitions (-> task-definition (.getContainerDefinitions))]
+    {:family                family
+     :revision              revision
+     :status                (keyword (.toLowerCase status))
+     :container-definitions (mapv container-definition->map container-definitions)}))
+
+;; Construct new AWS SDK objects
+
+(defn new-client
+  ([^AWSCredentialsProvider cred]
+   (AmazonECSClient. cred))
+  ([^AWSCredentialsProvider cred region]
+   (-> (AmazonECSClient. cred)
+       (.setRegion (Region/getRegion (Regions/fromName region))))))
+
+;; AWS SDK ECS Api
 
 (defn create-cluster
-  "Create new ECS cluster"
-  [^AmazonECSClient client name]
-  (-> (.createCluster client (-> (CreateClusterRequest.)
-                                 (.withClusterName name)))
-      (.getCluster)
-      (cluster->map)))
+  [^AmazonECSClient client cluster]
+  (let [ec2-create-request (-> (CreateClusterRequest.)
+                               (.withClusterName cluster))]
+    (-> client
+        (.createCluster ec2-create-request)
+        (.getCluster)
+        (cluster->map))))
 
 (defn delete-cluster
-  "Delete cluster"
-  [^AmazonECSClient client name]
-  (-> (.deleteCluster client (-> (DeleteClusterRequest.)
-                                 (.withCluster name)))
-      (.getCluster)
-      (cluster->map)))
+  [^AmazonECSClient client cluster]
+  (let [ec2-delete-request (-> (DeleteClusterRequest.)
+                               (.withCluster cluster))]
+    (-> client
+        (.deleteCluster ec2-delete-request)
+        (.getCluster)
+        (cluster->map))))
 
 (defn describe-cluster
   [^AmazonECSClient client name]
-  (let [result (.describeClusters client (-> (DescribeClustersRequest.)
-                                             (.withClusters [name])))
+  (let [ec2-describe-request (-> (DescribeClustersRequest.)
+                                 (.withClusters [name]))
+        result (-> client (.describeClusters ec2-describe-request))
         cluster (-> result (.getClusters) (seq) (first))
         failure (-> result (.getFailures) (seq) (first))]
     (cond
@@ -61,6 +94,41 @@
       (and failure (not cluster)) {:failure (-> failure (.getReason))}
       ;; Should not be here
       :else (throw (IllegalStateException. "Cluster is not found nor failed")))))
+
+
+(defn list-task-definition-families
+  [^AmazonECSClient client]
+  (-> client
+      (.listTaskDefinitionFamilies)
+      (.getFamilies)))
+
+(defn describe-task-definition
+  [^AmazonECSClient client family]
+  (let [ec2-describe-request (-> (DescribeTaskDefinitionRequest.)
+                                 (.withTaskDefinition family))]
+    (task-definition->map
+      (-> client
+          (.describeTaskDefinition ec2-describe-request)
+          (.getTaskDefinition)))))
+
+(defn register-task-definition
+  [^AmazonECSClient client family opts]
+  (let [{:keys [image cpu memory]} opts
+        ec2-container-def (-> (ContainerDefinition.)
+                              (.withImage image)
+                              (.withCpu (int cpu))
+                              (.withMemory (int memory))
+                              (.withName default-container-name))
+        ec2-register-request (-> (RegisterTaskDefinitionRequest.)
+                                 (.withFamily family)
+                                 (.withContainerDefinitions [ec2-container-def]))]
+    (log/debug "Register task definition. Family:" family "CPU:" cpu "Mem:" memory)
+    (task-definition->map
+      (-> client
+          (.registerTaskDefinition ec2-register-request)
+          (.getTaskDefinition)))))
+
+;; Combinators on top of AWS API
 
 (defn cluster-transition
   "Compute cluster state transition based on diff"
@@ -87,5 +155,5 @@
              (map (fn [state] (seq (sliding-window state))))
              (filter #(= 2 (count %)))
              (map (fn [[a b]] (cluster-transition a b))))]
-    (async/pipeline 1 transitions xf metronome)
+    (async/pipeline-blocking 1 transitions xf metronome)
     transitions))
